7e9be7c62b857206fe65e04125217a74c42c836e
sippo <sippo@DESKTOP-4013P7G>
2024-03-13 15:29:22 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lab7_2a
+{
+
+ 
+
+
+        internal class Program
+        {
+
+
+
+            static void Main(string[] args)
+            {
+                // Creates the Queue
+                // The data items are strings. The second type parameter (an int) indicates the priority
+                PriorityQueue<string, int> games = new PriorityQueue<string, int>();
+
+            // Add messages with different priorities to the queue
+            games.Enqueue("Zelda", 1);
+            games.Enqueue("Supermario", 3);
+            games.Enqueue("Finalfantasy", 1);
+            games.Enqueue("Madden", 2);
+            games.Enqueue("Contra", 4);
+
+            // Retreive the messages in priority and queue order
+            while (games.TryDequeue(out string item, out int priority))
+            {
+                Console.WriteLine($"Dequeued Item: {item} Priority was: {priority}");
+
+            }
+            
+
+
+                   }
+
+            
+        }
+    } 
+
